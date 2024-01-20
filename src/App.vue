3774997255a97c19ba87db494b5b6224dc28e8c9
Henrique Andrade <henriquenascandrade@outlook.com>
2024-01-19 22:18:37 -0300

    add Company component | set props object

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted } from 'vue';
+import Company from './components/Company.vue'
+import useCompanies from './composables/companies'
+
+const { companies, getCompanies } = useCompanies()
+
+onMounted(() => { getCompanies() })
+</script>
+
+<template>
+  
+  <Company v-for="company in companies" :key="company.id" :company="company" />
+</template>
