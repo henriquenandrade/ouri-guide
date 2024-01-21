de66f4a28055eb9530528ec27d76cea283ee42ad
Henrique Andrade <henriquenascandrade@outlook.com>
2024-01-21 19:27:18 -0300

    add method - get company by id

--- src/composables/companies.js
+++ src/composables/companies.js
@@ -2,15 +2,26 @@ import { ref } from "vue"
 import axios from "axios"
 
 export default function useCompanies() {
-    const companies = ref([])
+    const companies = ref()
+    const company = ref()
 
     const getCompanies = async () => {
         const response = await axios.get('https://ourikas.github.io/companies.json')
         companies.value = response.data
     }
 
+    const getCompany = async (id) => {
+        const response = await axios.get('https://ourikas.github.io/companies.json')
+        companies.value = response.data
+        company.value = companies.value.filter(el => {
+            return el.id == id
+        })
+    }
+
     return {
         companies,
-        getCompanies
+        company,
+        getCompanies,
+        getCompany
     }
 }
