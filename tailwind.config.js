f2590b4c21e64d03457da16898d2992a14eb0fa9
Henrique Andrade <henriquenascandrade@outlook.com>
2024-01-26 00:00:41 -0300

    container center plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
