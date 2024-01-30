74fc506636a483c1446b5e6ddd89951699be511d
Henrique Andrade <henriquenascandrade@outlook.com>
2024-01-29 23:54:56 -0300

    set api key in vite config and use google maps package in main

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 const app = createApp(App)
 
 app.use(router)
-app.mount('#app')
+app.use(VueGoogleMaps, {
+    load: {
+        key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    },
+}).mount('#app')
