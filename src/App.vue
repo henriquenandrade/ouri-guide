ce01ecdc50af60d02a58f38cd0f39f374881eddd
Henrique Andrade <henriquenascandrade@outlook.com>
2024-08-25 15:47:33 -0300

    refactor code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import Company from './components/Company.vue'
+import { onMounted } from 'vue';
 import useCompanies from './composables/companies'
 import { RouterView } from 'vue-router'
 
-const search = ref('')
-const filteredCompanies = ref([])
-
-const { companies, getCompanies } = useCompanies()
-
-function searchCompany() {   
-   filteredCompanies.value = companies.value.filter((el) => {
-    return el.name.toLowerCase().includes(search.value.toLowerCase())
-   })
-}
+const { getCompanies } = useCompanies()
 
 onMounted(() => { getCompanies() })
 </script>
